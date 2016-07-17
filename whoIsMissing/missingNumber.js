5efc3e561ca54542ede04716f094746b7a39be8e
farahamawi <amawifarah@gmail.com>
2016-07-17 08:56:40 +0300

    missing number

--- whoIsMissing/missingNumber.js
+++ whoIsMissing/missingNumber.js
@@ -9,13 +9,26 @@
 // EXAMPLE: When inputArray is [2,3,4,6], output should be 5
 
 var orderedArrayMissingNumber = function(orderedInputArray) {
-	// Your code here..
+	for (var i = 0; i < orderedInputArray.length; i++) {
+		if(orderedInputArray[i+1]!==orderedInputArray[i]+1){
+			return orderedInputArray[i]+1
+		}
+	}
 }
-
+//to solve it in constant time 
+var orderedInputArray2 = function(arr){
+	
+}
 // Input will not be in order.
 // EXAMPLE: When inputArray is [6,5,7,3,2], output should be 4.
 
 // What is the time complexity of your solution?
-var unorderedArrayMissingNumber = function(unorderedInputArray) {
-	// Your code here..
+var unorderedArrayMissingNumber = function(array) {
+	for (var i = 0; i < array.length; i++) {
+		if(array[i]!== Math.max.apply(Math,array))
+		if(array.indexOf(array[i]+1)===-1){
+			return array[i]+1
+		}
+	}
 }
+// time complexity = O(n^2)
