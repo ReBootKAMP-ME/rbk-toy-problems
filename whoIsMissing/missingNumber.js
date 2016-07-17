2649fa91365bb1070ecf934be80fdc2fbe883ec9
housamaldeen <housam993@gmail.com>
2016-07-17 08:22:16 +0300

    Implement missingNumber functions

--- whoIsMissing/missingNumber.js
+++ whoIsMissing/missingNumber.js
@@ -9,13 +9,26 @@
 // EXAMPLE: When inputArray is [2,3,4,6], output should be 5
 
 var orderedArrayMissingNumber = function(orderedInputArray) {
-	// Your code here..
+	var currentNumber = orderedInputArray[0];
+	for (var i = 0; i < orderedInputArray.length-1; i++) {
+		if(orderedInputArray.indexOf(++currentNumber) === -1){
+			return currentNumber;
+		}
+	}
 }
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
 
 // Input will not be in order.
 // EXAMPLE: When inputArray is [6,5,7,3,2], output should be 4.
 
-// What is the time complexity of your solution?
+// What is the time complexity of your solution? O(n)
 var unorderedArrayMissingNumber = function(unorderedInputArray) {
-	// Your code here..
+	var currentNumber = unorderedInputArray.min();
+	for (var i = 0; i < unorderedInputArray.length-1; i++) {
+		if(unorderedInputArray.indexOf(++currentNumber) === -1){
+			return currentNumber;
+		}
+	}
 }
