7fb013dc438217c86d84f0e33d22eba6a22993ba
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-17 09:21:40 +0400

    Find missing number in ordered and non ordered array

--- whoIsMissing/missingNumber.js
+++ whoIsMissing/missingNumber.js
@@ -7,15 +7,47 @@
 // The input will be in order.
 // Try to solve with O(1) time complexity
 // EXAMPLE: When inputArray is [2,3,4,6], output should be 5
-
 var orderedArrayMissingNumber = function(orderedInputArray) {
-	// Your code here..
+	var missing;
+	var l = array.length
+	var first = array[0]
+	for (var i=0; i<l; i++){
+		if (array[i] > i+first){
+			missing = i;
+			break;
+		}
+	}	
+	return missing;	
 }
 
 // Input will not be in order.
 // EXAMPLE: When inputArray is [6,5,7,3,2], output should be 4.
 
 // What is the time complexity of your solution?
+var order = function (unOrderedArray) {
+	var sol;
+	var solution = function (array){
+		var switched = false;
+		var temp;
+		for (var i=0; i<array.length-1; i++){
+			if (array[i]>array[i+1]){
+				temp = array[i+1];
+				array[i+1] = array [i];
+				array[i] = temp;
+				switched = true
+			}
+		}
+		if (!switched) {
+			sol = array
+		} else {
+			solution (array);
+		}
+	}
+	solution (unOrderedArray);
+	return array;
+}
 var unorderedArrayMissingNumber = function(unorderedInputArray) {
-	// Your code here..
+	var ordered = order(unorderedInputArray);
+	var missing = orderedArrayMissingNumber(ordered);
+	return missing;
 }
