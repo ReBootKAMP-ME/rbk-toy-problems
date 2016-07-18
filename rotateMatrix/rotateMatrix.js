1b92a67d2f937cd70e1f4662ba46b108d1a21b74
MohannadBakkar <mohannadbakkar@gmail.com>
2016-07-18 08:56:09 +0300

    Toy problem

--- rotateMatrix/rotateMatrix.js
+++ rotateMatrix/rotateMatrix.js
@@ -38,5 +38,27 @@
  */
 
 var rotateMatrix = function(matrix){
+    var n = matrix.length;
+    for (var i = 0; i < n / 2; i++) {
+        for (var j = 0; j < Math.ceil(n/2); j++) {
+            var temp = matrix[i][j];
+            matrix[i][j] = matrix[n-1-j][i];
+            matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+            matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+            matrix[j][n-1-i] = temp;
+        }
+    }
+    return matrix;
+	
+	// var x = [];
+	// for (var i = 0; i < matrix.length; i++) {
+	// 	var y = matrix[i]
+	// 	for (var j = 0; j < y.length; j++) {
+	// 		x.unshift(y[j])
 
+
+			
+	// 	}
+	// }
+	// return x;
 };
