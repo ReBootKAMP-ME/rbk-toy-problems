c6d96058cea31ac4d0e0575668f35d1e2a51c29e
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-07-23 09:14:53 +0300

    Done

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,4 +8,25 @@
  */
 
 var largestProductOfThree = function(array) {
+	// var arr=array;
+	// max1 =Max(array).max;
+	// max2 =Max(array.splice(Max(array).index,1));
+	// max3 =Max(array.splice(Max(array).index,1));
+	// return max1*max2*max3;
+
+	var arr=array.sort();
+	var length=array.length-1;
+	return arr[length]*arr[length-1]*arr[length-2];
 };
+
+// function Max(arr) {
+// 	var result={};
+// 	result.max=arr[0];
+// 	for (var i = 1; i < arr.length; i++) {
+// 		if(arr[i]>result.max){
+// 			result.max=arr[i];
+// 			result.index=i;
+// 		}
+// 	}
+// 	return result;
+// }
