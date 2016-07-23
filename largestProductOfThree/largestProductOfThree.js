fdb7461b50901d885278109e3bf5ba9fe5247c64
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-23 08:58:41 +0400

    Make Toy Problem

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,4 +8,26 @@
  */
 
 var largestProductOfThree = function(array) {
+	var arrayOfIndexes = [];
+	var flag = true;
+	var comp;
+	var index;
+	while (flag){
+		comp = -Infinity;
+		for (var i=0; i<array.length; i++){
+			if (array[i]>comp && arrayOfIndexes.indexOf(i) === -1){
+				index = i;
+				comp = array[i];
+			}
+		}
+		arrayOfIndexes.push(index);
+		if (arrayOfIndexes.length === 3){
+			flag = false
+		}
+	}
+	var product = array [arrayOfIndexes[0]];
+	for (var j=1; j<arrayOfIndexes.length; j++){
+		product = product * array[arrayOfIndexes[j]]
+	};
+	return product
 };
