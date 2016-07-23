80e3448b0d9b1b0c47aa9af93da518a1169d26a7
farahamawi <amawifarah@gmail.com>
2016-07-23 08:47:01 +0300

    largest product of three

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,4 +8,18 @@
  */
 
 var largestProductOfThree = function(array) {
+	var arr =[];
+	var array2 =array;
+	for (var i = 0; i < 3; i++) {
+		var max = Math.max.apply(Math,array2)
+		arr.push(max)
+		var maxIndex = array2.indexOf(max)
+		array2.splice(maxIndex,1)
+	}
+	var result =1;
+	for (var i = 0; i < 3; i++) {
+		result=result*arr[i]
+	}
+	return result
+	
 };
