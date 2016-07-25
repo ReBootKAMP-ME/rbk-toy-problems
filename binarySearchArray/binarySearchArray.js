bce3b10aed094c9c2f896210b65a5c05663487f3
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-25 08:21:07 +0300

    Solved the binary search array problem

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,17 @@
  */
 
 var binarySearch = function (array, target) {
-  
+  if(array.length === 1){
+  	if(array[0]=== target){
+  		return 0;
+  	} else {
+  		throw "Error: Target Not in the array";
+  	}
+  }
+  var midIndex = Math.floor(array.length / 2);
+  if(array[midIndex]<=target){
+  	return midIndex+binarySearch(array.slice(midIndex),target);
+  }
+  return binarySearch(array.slice(0,midIndex),target);
 };
 
