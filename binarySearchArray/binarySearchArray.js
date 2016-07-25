bb5e5b5756dc1b5e3641968af8a075c9441eaed8
RaghadAlkhawaldeh <raghadkhawaldeh@gmail.com>
2016-07-25 08:46:21 +0300

    BinarySearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,33 @@
  */
 
 var binarySearch = function (array, target) {
+	var min = 0;
+	var max = array.length-1;
+	var mid = Math.floor((min+max)/2);
   
+  while(array[mid] !== target ){
+  		if(target<array[mid]){
+  			max = mid-1;
+  		}else if (target > array[mid]){
+            min = mid + 1;
+        }
+     mid= Math.floor((min+max)/2);
+  }
+  return mid;
 };
+
+//   var func = function(mid, target){
+//   if(array[mid]===target){
+//   	return mid;
+//   }else if(array[mid]<target){
+//   		min=mid+1; 
+// 			mid=Math.floor((min+max)/2);
+//   	}else{
+//   		mix=mid-1;
+//   		mid=Math.floor((min+max)/2);
+//   	}
+// }
+// 	func(mid,target)
+// }
+
 
