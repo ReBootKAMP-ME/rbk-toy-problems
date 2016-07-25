e96590dbaa635156e37b14ec55f5202c1a4e56ab
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2016-07-25 08:52:45 +0300

    toy

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -8,9 +8,37 @@
  *
  * var index = binarySearch([1, 2, 7, 15, 27], 27);
  * console.log(index); // 4
+ b7bkkk
  */
 
-var binarySearch = function (array, target) {
-  
-};
+// var binarySearch = function (array, target) {
+// 	var index=0
+// 	for (var i = 0; i < array.length; i++) {
+// 		if(array[i]=== target)
+// 			index= i;
+// 	}
+//   return index ;
+// };
 
+
+
+var binarySearch = function(array , target) {
+    var firstIndex = 0;
+    var lastIndex = array.length - 1;
+    var midIndex =(array.length-1)/2
+    	for (var i = firstIndex ; i <= lastIndex; i++) {
+    		if(target < array[midIndex]){
+    			lastIndex = midIndex -1 ; 
+    			midIndex= Math.round((lastIndex+firstIndex)/2)
+    		}
+    		else if(target> array[midIndex]){
+    			firstIndex = midIndex +1 ;
+    			midIndex=  Math.round((lastIndex+firstIndex)/2) ;
+    		}
+    		else if(target = array[midIndex]){
+    			return midIndex;
+    		}
+
+    	}
+    	return midIndex
+}
