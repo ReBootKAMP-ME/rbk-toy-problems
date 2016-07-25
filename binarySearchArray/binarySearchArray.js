0c52e8ae994d76b4784cde3c9ca13c239abfec59
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-25 08:38:27 +0300

    finished

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,20 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
-  
+var binarySearch = function (array, target, index) {
+  var middle = middle || Math.floor(array.length/2);
+  var index = index || middle;
+	  if(array[middle] === target){
+	  	return index;
+	  }else if (array[middle]< target && array.length>1){
+	  	index = Math.floor(index + index/2)
+	  	return binarySearch(array.slice(middle), target, index)
+	  }else if (array[middle]>target && array.length>1){
+	  	index = Math.floor(index/2);
+	  	return binarySearch(array.slice(0,middle), target, index)
+	  }
+	  else{
+	  	console.log("Target not Found");
+	  }
 };
 
