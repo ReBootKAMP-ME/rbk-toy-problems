5b499d1e6310f736182f3de25bd1958936787d44
eshraq1991 <bakri.eshraq@gmail.com>
2016-07-26 08:44:15 +0300

    LinkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -19,21 +19,55 @@
 
 var LinkedList = function(){
   //fill me in!
+  //this.storage={};
+  this.head="null";
+  this.tail="null";
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+	var newNode=this.makeNode(value);
+	if(this.head==="null"){
+		this.head=newNode;
+		this.tail=newNode;
+		this.head.next=this.tail;
+	}else{
+		this.tail.next=newNode;
+		this.tail=this.tail.next;
+	}
 };
 
 LinkedList.prototype.removeHead = function(){
+	if(this.head.value !== null){
+		var temp=this.head;
+		this.head=this.head.next;
+		delete temp;
+		return temp.value;
+	}
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+	var nodeG=this;
+	var exist=false;
+	var rec=function(node){
+		if(node.next!==null){
+			if(node.value===value){
+				exist=true;
+				return;
+			}else{
+				node=node.next;
+				rec(node);
+			}
+		}else{
+			return;	
+		}
+	}
+	rec(nodeG);
+	return exist;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+	this.value=value;
+	this.next={};
 };
