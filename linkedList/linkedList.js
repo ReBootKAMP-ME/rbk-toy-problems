062d6de8ed7a6508452413601060fef9865dc633
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-07-26 08:57:41 +0300

    Never been good at this , time to understand it more

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -19,21 +19,65 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
+// never got comfortable with this
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+	// if our head is null 
+	// then our node will be in the head
+	// and our tail would be the next 
+	// but if our head is not null
+	// then we want to add to the tail a new node
+	// how do we set up the next to take the next object
+	// we want first to set 
+	if(this.head === null){
+		var node = new this.makeNode(value);
+		this.head = node;
+		this.tail = node;
+	} else {
+		var x = this.tail;
+		var node = new this.makeNode(value);
+		this.tail = node;
+		this.head.next = node;
+	}
 };
+
 
 LinkedList.prototype.removeHead = function(){
+	this.head = this.head.next;
+	return this.head.value;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value , head){
+	// define the head to a variable 
+	var list = this.head;
+	// while false
+	while(list.value !== value){
+		if(list.next !== null){
+			list = list.next;
+		} else {
+			return false;
+		}
+	}
+	// if our list's value is true it wont enter the while loop and will return true
+	return true;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+	// node will be an object that has next 
+	// and a value MAYBE!
+ 	this.value = value;
+ 	this.next = null;
 };
+
+
+
+
+
+
+
+
