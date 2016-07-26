ff6ba6e802ed6ab0b88c2be059e66e3d3e708852
farahamawi <amawifarah@gmail.com>
2016-07-26 08:58:40 +0300

    linked-list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,22 +18,55 @@
 
 
 var LinkedList = function(){
+	this.value = {};
+	this.next = null;
+	this.tail = null;
+	this.head = null;
+	this.length = 0;
   //fill me in!
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+	if(this.tail === null ){
+		this.value = value;
+		this.head = this.value;
+		this.tail = this.value;
+		this.length++
+	}
+	this.tail.next = {this.value :value,
+				 	  this.value : null
+					 }
+	this.tail = this.tail.next;
+	this.length++;
+
 };
 
 LinkedList.prototype.removeHead = function(){
+	var head = this.head;
+	this.head = this.head.next;
+	this.length--;
+	return head;
+	
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+
+	var containValue = function(node){
+
+		if (this.head.value === value){
+			return true;
+		}
+		if ( this.head.next.value === value){
+			return true
+		}
+		
+	}
+	
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+	// this.value = value;
+	// this.next = null;
 };
