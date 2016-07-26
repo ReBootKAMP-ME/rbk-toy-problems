5bb2a40e3d176fb310312c6fc43dca2e6bfb4ddd
Fatima Hammami <en.f.h@live.com>
2016-07-26 08:56:38 +0300

    linkedlist

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -19,21 +19,59 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.tail={};
+  this.head={};
+  this.node={};
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+	//check if there is no head then make the tail value is the head value
+	if(this.head === null){
+		this.head=this.makeNode(value);
+		this.tail=this.head;
+	}else{
+		//make the current tail points to a new node value 
+		//and make the current tail point to this new node's value
+		this.tail.next=this.makeNode(value);
+		this.tail=this.node.value;
+	}
 };
 
 LinkedList.prototype.removeHead = function(){
+	if(this.head === null){
+		return;
+	}
+	//check if the head is the tail then its one node list
+	if(this.head === this.tail){
+		var head=this.head.value;
+		this.head.value=null;
+		return head;
+	}else{
+		//make the head is the next node value after removing the current one
+		var head=this.head.value;
+		this.head=this.node.next;
+		return head;		
+	}
+	
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+	if(this.head === value || this.tail === value){
+		return true;
+	}else{
+		for(var keys in this.node){
+			if(this.node[value]){
+				return true;
+			}
+		}
+	}
+	return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+	this.node.value=value;
+	this.node.next=null;
+	return this.node;
 };
