ba81afd1e8f269e9741ce0f7637fc74337ad4363
farahamawi <amawifarah@gmail.com>
2016-07-30 10:09:22 +0300

    deep equals for 2 obj

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,31 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(obj1, obj2){
+var deepEquals = function(obj1, obj2){	
+	
+		var obj1keys = Object.keys(obj1);
+		var obj2keys = Object.keys(obj2);
+		//check if we they have same keys
+		for (var i = 0; i < obj1keys.length; i++) {
+			if(obj1keys[i]!== obj2keys[i]){
+				return false;
+			}
+		}
+	
+	
+
+		var obj1keys = Object.keys(obj1);
+		var obj2keys = Object.keys(obj2);
+		//check if they have same value for each key 
+		for (var i = 0; i < obj1keys.length; i++) {
+			//if they have nested objects // recursion
+			// if(typeof(obj1.obj1keys[i])==="object"){
+			// 	deepEquals(obj1[obj1keys[i]],obj2[obj2keys[i]])
+			// }
+			if(obj1[obj1keys[i]] !== obj2[obj2keys[i]]){
+				return false;
+			}
+		}
+	
+	return true;
 };
