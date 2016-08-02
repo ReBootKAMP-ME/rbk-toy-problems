84d2a3712f0ec68fae76b60939a0e61789a8a6d0
Mihyar <eng.mihyear@gmail.com>
2016-08-02 09:48:22 +0300

    telephoneletters

--- telephoneLetters/telephoneLetters.js
+++ telephoneLetters/telephoneLetters.js
@@ -27,3 +27,37 @@
   *  Why not filter your results to only return words contained in that file?
   *
   */
+
+	// var num2 = ["A","B","C"];
+	// var num3 = ["D","E","F"];
+	// var num4 = ["G","H","I"];
+	// var num5 = ["J","K","L"];
+	// var num6 = ["M","N","O"];
+	// var num7 = ["P","Q","R","S"];
+	// var num8 = ["T","U","V"];
+	// var num9 = ["W","X","Y","Z"];
+
+var keypad = ["0","1",["A","B","C"],["D","E","F"],["G","H","I"],["J","K","L"],
+			 ["M","N","O"],["P","Q","R","S"],["T","U","V"],["W","X","Y","Z"]];
+
+var telephoneWords = function (digits){
+	var result = [];
+	digits = digits.toString().split("");
+		var num1 = keypad[digits[0]];
+		var num2 = keypad[digits[1]];
+		var num3 = keypad[digits[2]];
+		var num4 = keypad[digits[3]];
+		for (var i = 0; i < num1.length; i++) {
+			for (var j = 0; j < num2.length; j++) {
+				for (var k = 0; k < num3.length; k++) {
+					for (var l = 0; l < num4.length; l++) {
+						var word = num1[i]+num2[j]+num3[k]+num4[l];
+						result.push(word);
+					}
+				}
+			}
+		}
+
+
+	return result;
+}
