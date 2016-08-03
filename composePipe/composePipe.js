8f163f7cac8d85398438b1845c5c03ef81b0fd1f
Mihyar <eng.mihyear@gmail.com>
2016-08-03 09:52:44 +0300

    composePipe

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -11,8 +11,8 @@
  * You can view Compose as moving RIGTH to LEFT through its arguments.
  *
  * Compose Example:
-   var greet = function(name){ return 'hello ' + name;}
-   var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+   var greet = function(name){ return 'hello ' + name;};
+   var exclaim = function(statement) { return statement.toUpperCase() + '!';};
    var welcome = compose(greet, exclaim);
    welcome('phillip'); // 'hello PHILLIP!'
 
@@ -36,10 +36,22 @@
 
 'use strict';
 
-var compose = function(){
-
+var compose = function(func1,func2){
+	var args = Array.prototype.slice.call(arguments);
+		return function(statement){
+			for (var i = args.length - 1; i >= 0; i--) {
+				statement = args[i].call(this,statement);
+			}
+			return statement;
+		}
 };
 
 var pipe = function(){
-
+	var args = Array.prototype.slice.call(arguments);
+	return function (statement) {
+		for (var i = 0; i < args.length; i++) {
+			statement = args[i].call(this,statement)
+		}
+		return statement;
+	}
 };
