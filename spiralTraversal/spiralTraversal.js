dc479853b3a529cbab8b60bcb0ea5176f3a8d504
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-06 09:58:24 +0300

    spiraltraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -13,6 +13,35 @@
  */
 
 var spiralTraversal = function(matrix){
-
+   var arrayResult=[];
+   while(true){
+   	if(matrix.length===0){
+   		break;
+   	}
+   for(var i=0;i<matrix.length;i++){
+   	  if(i===0){
+   	  	for(var j=0;j<matrix[i].length;j++){
+   	  		arrayResult.push(matrix[i][j]);
+   	  	}
+   	  	for(var w=1;w<matrix.length;w++){
+   	  		arrayResult.push(matrix[w][matrix.length-1]);
+   	  		matrix[w].pop();
+   	  	}
+   	  	matrix.splice(0,1);
+   	  	console.log(matrix,"1");
+   	  }else if(i===matrix.length-1){
+   	  	for(var k=matrix[i].length-2;k>=0;k--){
+   	  		arrayResult.push(matrix[i][k]);
+   	  	}
+   	  	for(var y=matrix.length-2;y>0;y--){
+   	  		arrayResult.push(matrix[y][i]);
+   	  		matrix[y].shift()
+   	  	}
+   	  	matrix.splice(matrix.length-1);
+   	  	console.log(matrix,"a5eir");
+   	  }
+   }
+}
+   return arrayResult;
 };
 
