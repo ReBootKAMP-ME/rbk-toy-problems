1e5c1d50a1f4c620845d2908cbcada7667cf7c6d
farahamawi <amawifarah@gmail.com>
2016-08-07 09:52:00 +0300

    find all

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,31 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
+	var n = arr.length;
+	var result = [];
+	var index = 0;
+	var index2 = 0;
+	var counter = 1;
+	var func = function () {
+		if(arr[index]=== arr[index2]){
+			counter++;
+		}
+		if(counter-1 > Math.floor(n/3)){
+			result.push(arr[index]);
+			index++;
+			counter=1;
+			func();
+		}
+		if(index2!== arr.length-1){
+			index2++;
+			func()
+		}
+		if(index!==arr.length-1){
+			index++;
+			counter = 1;
+			func();
+		}
 
+	}
+	return result;
 }
