9e03ed24139b16d5dd5143ce419846ec346cf98f
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-07 09:19:09 +0400

    Find all majority elements

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -8,6 +8,21 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 
 */
 
-var findAll = function(arr){
-
-}
+var findAll = function(arr) {
+	var n = arr.length / 3;
+	var l = arr.length
+	var obj_rep = {};
+	var outArr = [];
+	for ( var i = 0; i < l; i++ ) {
+		if (obj_rep[arr[i]] && obj_rep[arr[i]] !== 'done') {
+			obj_rep [arr[i]]++;
+			if (obj_rep [arr[i]] > n){
+				outArr.push(arr[i]);
+				obj_rep[arr[i]] = 'done';
+			}
+		} else if (!obj_rep[arr[i]]) {
+			obj_rep[arr[i]] = 1;
+		}
+	}
+	return outArr;
+};
