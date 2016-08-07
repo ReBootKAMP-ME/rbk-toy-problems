244567c7df78600fc919ab5d5bb6f8de03d0c6f0
Fatima Hammami <en.f.h@live.com>
2016-08-07 09:25:26 +0300

    majority elment

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,21 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
+var numbers={};
+var results=[];
+var n=Math.floor(arr.length / 3);
 
+for (var i = 0; i < arr.length; i++) {
+	if(!!numbers[arr[i]]){
+		numbers[arr[i]].push(arr[i]);
+	}else{
+		numbers[arr[i]]=[];
+		numbers[arr[i]].push(arr[i]);
+	}
+}
+for(var key in numbers){
+	if(numbers[key].length > n)
+		results.push(numbers[key][0])
+	}
+return results;
 }
