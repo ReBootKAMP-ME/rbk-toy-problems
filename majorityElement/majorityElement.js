51c612040d2aab74509ac06b2f4d74797efe6ed4
Saif Elokour <saiffelokour@gmail.com>
2016-08-07 09:31:50 +0300

    solves majorityElement

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,23 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
+	var n = arr.length;
+	var pass = n/3;
+	var elements = {}
+	var result = new Set()
 
+	for(var i = 0; i < n; i++){
+
+		if(!elements[arr[i]]){
+			elements[arr[i]] = 1
+		} else {
+			elements[arr[i]] += 1
+		}
+
+		if(elements[arr[i]] > pass){
+			result.add(arr[i])
+		}
+	}
+
+	return result
 }
