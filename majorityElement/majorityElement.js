d0024a314948a25f5f63f64db9bd437f59bf7630
Hadeel <hadylgk87@gmail.com>
2016-08-07 09:55:53 +0300

    updated

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,19 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
-
+	var occurenace = Math.floor(arr.length/3);
+	var counter = 0;
+	var possibleElement = arr[0];
+	arr.splice(0,1);
+	if(arr.indexOf(possibleElement)>0){
+		counter++;
+		if(counter === occurenace)
+			return possibleElement;
+		var temp = arr.indexOf(possibleElement);
+		arr.splice(temp,1);
+	}
+	else{
+		// recursion;
+		return findAll(arr);
+	}
 }
