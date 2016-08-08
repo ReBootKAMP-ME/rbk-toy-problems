23f5fb0d11bf6167761e1724e5690bac16f0fa9d
AwsAhmed <aws.y.ahmed@gmail.com>
2016-08-08 09:53:08 +0300

    --

--- pascalTriangle/pascalTriangle.js
+++ pascalTriangle/pascalTriangle.js
@@ -11,6 +11,7 @@ For example, the pascal triangle with 4 rows is:
     1   2   1
   1   3   3   1
 
+
 or in array form: [[1],[1,1],[1,2,1],[1,3,3,1]];
 
 Read more about Pascal Triangle here:
@@ -24,6 +25,19 @@ For example:
 buildTriangle(3);// should output [[1],[1,1],[1,2,1]];
 */
 
+// create array with [[1],[1,1]]
+// each time we push array start with 1 and end with one in the middle 
 var buildTriangle = function(numOfRows){
-
+	var result = new Array(numOfRows);
+	result[0]=[1]
+	result[1]=[1,1]
+	for (var i = 2; i < result.length; i++) {
+		result[i]=new Array(i+1)
+		result[i][0]=1
+		result[i][result[i].length-1]=1;
+		for (var j = 1; j < result[i].length-1; j++) {
+			result[i][j] =  
+		}
+	}
+return result;
 }
