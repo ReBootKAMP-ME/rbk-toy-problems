830e91c3d6206288bf5af1c82319b603c89ddc04
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-09 10:18:44 +0300

    solved the robotPaths problem

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -34,7 +34,50 @@ var makeBoard = function(n) {
 //var myBoard = makeBoard(5);
 //The answer for a board size 5 is 8512 unique paths
 
-var robotPaths = function(/*you can pass any parameters you need*/) {
+var copyBoard = function(board){
+	var newBoard = makeBoard(board.length);
+	for(var i=0; i<newBoard.length; i++){
+		for(var j=0; j<newBoard[i].length; j++){
+			newBoard[i][j] = board[i][j];
+		}
+	}
+	return newBoard;
+};
 
-}
+var robotPaths = function(board) {
+	var pathsCounter = 0;
+
+	var getPaths = function(board, rowIndex, colIndex){
+
+		board.togglePiece(rowIndex,colIndex);
+		if(board[board.length-1][board.length-1]===true){
+			pathsCounter++;
+		} else {
+			// go up
+			var newBoard = copyBoard(board);
+			if( rowIndex>0 && newBoard[rowIndex-1][colIndex]!==undefined && !newBoard.hasBeenVisited(rowIndex-1,colIndex)){
+				getPaths(newBoard, rowIndex-1, colIndex);
+			}
+			// go down
+			var newBoard = copyBoard(board);
+			if( rowIndex<newBoard.length-1 && newBoard[rowIndex+1][colIndex]!==undefined && !newBoard.hasBeenVisited(rowIndex+1,colIndex)){
+				getPaths(newBoard, rowIndex+1, colIndex);
+			}
+			// go left
+			var newBoard = copyBoard(board);
+			if( colIndex>0 && newBoard[rowIndex][colIndex-1]!==undefined && !newBoard.hasBeenVisited(rowIndex,colIndex-1)){
+				getPaths(newBoard, rowIndex, colIndex-1);
+			}
+			// go right
+			var newBoard = copyBoard(board);
+			if( colIndex<newBoard.length-1 && newBoard[rowIndex][colIndex+1]!==undefined && !newBoard.hasBeenVisited(rowIndex,colIndex+1)){
+				getPaths(newBoard, rowIndex, colIndex+1);
+			}
+		}
+	}
+
+	getPaths(board, 0, 0);
+
+	return pathsCounter;
+};
 
