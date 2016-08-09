79628fb13ca8bac4480925788cc018632623c002
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-09 09:46:32 +0400

    Find all paths to freedom

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -10,8 +10,30 @@
  *  Extra credit: Make your solution work for a grid of any size.
  *
  */
+ /*
+  Input = size of board
+  output = number of unique paths
+  Rules:
+  1- cannot visit the same spot twice per path
+  2- can only move up (vertical position - 1), down (v + 1), left (h - 1), right (h + 1)
+  3- path is top left to buttom right
 
+  Functions to benefit from:
+  1- togglePiece; which will mimic the robot movement
+  2- a path is the toggled pieces on the way
+  3- hasBeenVisited; to make sure a spot was not visited;
+  
+  - From the start, this seems as a depth first search, where each path is determined before moving to the next one
+  - So, for position 0,0 the robot has two choices, move right or down, each one is a branch of a decision tree
+            ________ 0,0 _________
+            |                     |
+    ________0,1______     _______1,0_________
+  0,2               1,1   1,1               2,0
+            
+ edges of board, when position is < 0 or > boardSize
+ */
 // A Board class will be useful. You can add extra methods to it.
+
 
 var makeBoard = function(n) {
   var board = [];
@@ -34,7 +56,32 @@ var makeBoard = function(n) {
 //var myBoard = makeBoard(5);
 //The answer for a board size 5 is 8512 unique paths
 
-var robotPaths = function(/*you can pass any parameters you need*/) {
-
-}
+var robotPaths = function(n) {
+	var myBoard = makeBoard(n);
+	var numberOfPaths = 0;
+	var solution = function (i, j) {
+		if (i === n - 1 && j === n - 1) {
+			numberOfPaths++;
+			return;
+		} else {
+			myBoard.togglePiece(i, j);
+			if (i !== n - 1 && !myBoard.hasBeenVisited(i + 1, j)) {
+				solution (i + 1, j);
+			}
+			if (i !== 0 && !myBoard.hasBeenVisited(i - 1, j)) {
+				solution (i - 1, j);
+			}
+			if (j !== n - 1 && !myBoard.hasBeenVisited(i, j + 1)) {
+				solution (i, j + 1);
+			}
+			if (j !== 0 && !myBoard.hasBeenVisited(i, j - 1)) {
+				solution (i, j - 1);
+			}
+			myBoard.togglePiece(i, j);
+			return;
+		}
+	};
+	solution(0,0)
+	return numberOfPaths;
+};
 
