64f42b2da2db2fd15d113ff85a4ea3e3f4ff99f9
Saif Elokour <saiffelokour@gmail.com>
2016-08-09 10:18:34 +0300

    incomplete

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -34,7 +34,93 @@ var makeBoard = function(n) {
 //var myBoard = makeBoard(5);
 //The answer for a board size 5 is 8512 unique paths
 
-var robotPaths = function(/*you can pass any parameters you need*/) {
+var robotPaths = function(boardSize) {
+  boardSize = 5
+  var myBoard = makeBoard(boardSize)
 
+  //robot initial start
+  myBoard.togglePiece(0,0);
+
+  var moveLeft = function(robot, i, j){
+    if(j === 0){
+      return 'DEAD END'
+    }
+
+    robot.togglePiece(i, j - 1)
+    return j - 1
+  }
+
+  var moveRight = function(robot, i, j){
+    if(j === boardSize - 1){
+      return 'DEAD END'
+    }
+
+    robot.togglePiece(i, j + 1)
+    return j + 1
+  }
+
+  var moveUp = function(robot, i, j){
+    if(i === 0){
+      return 'DEAD END'
+    }
+
+    robot.togglePiece(i - 1, j)
+    return i - 1
+  }
+
+  var moveDown = function(robot, i, j){
+    if(i === boardSize - 1){
+      return 'DEAD END'
+    }
+
+    robot.togglePiece(i + 1, j)
+    return i + 1
+  }
+
+  var move = function(dir, robot, i, j){
+    if(dir === 'LEFT'){
+      return moveLeft(robot, i, j)
+
+    } else if(dir === 'RIGHT'){
+      return moveRight(robot, i, j)
+
+    } else if(dir === 'UP'){
+      return moveUp(robot, i, j)
+      
+    } else if(dir === 'DOWN'){
+      return moveDown(robot, i, j)
+      
+    }
+  }
+
+  var createPath = function(newPath, board, i, j){
+
+    var direct = 'LEFT'
+
+    while(!board.hasBeenVisited(boardSize-1, boardSize-1)){
+      
+      var temp = move(direct, board, i, j)
+
+      if(temp !== 'DEAD END' && direct === ('LEFT' || 'RIGHT')){
+        j = temp;
+      } else if (temp !== 'DEAD END' && direct === ('UP' || 'DOWN')){
+        i = temp
+      } else {
+        
+      }
+
+    }
+
+    return newPath
+  }
+
+  var everyPath = function(paths, numOfPaths, i, j, newPath){
+    
+    createPath([], myBoard)
+
+    return everyPath(paths, numOfPaths + 1, 0, 0, [])
+  }
+
+  return everyPath([], 0, 0, 0, []) 
 }
 
