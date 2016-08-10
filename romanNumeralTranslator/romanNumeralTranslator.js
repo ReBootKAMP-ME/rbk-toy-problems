1d623e84f75161c08e6cbddf05ec36990c71d6e6
Saif Elokour <saiffelokour@gmail.com>
2016-08-10 09:39:35 +0300

    solves romanNumerals

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -16,16 +16,33 @@
  * You should return `null` on invalid input.
  */
 
-var DIGIT_VALUES = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
-};
 
 var translateRomanNumeral = function(romanNumeral){
+	var DIGIT_VALUES = {
+	  I: 1,
+	  V: 5,
+	  X: 10,
+	  L: 50,
+	  C: 100,
+	  D: 500,
+	  M: 1000
+	};
+	var numarals = romanNumeral.split('')
+	var result = 0;
+	var temp;
 
+	for(var i = 0; i < numarals.length; i++){
+		
+		if(DIGIT_VALUES[numarals[i]] < DIGIT_VALUES[numarals[i+1]]){
+			temp = -DIGIT_VALUES[numarals[i]]
+		} else if (DIGIT_VALUES[numarals[i]] && DIGIT_VALUES[numarals[i+1]] < DIGIT_VALUES[numarals[i+2]]){
+			return null
+		} else {
+			temp = DIGIT_VALUES[numarals[i]]
+		}
+
+		result += temp
+	}
+
+	return result
 };
