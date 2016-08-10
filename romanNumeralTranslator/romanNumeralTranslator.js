732aecd5046880745113ca9305c233e211b35485
farahamawi <amawifarah@gmail.com>
2016-08-10 09:39:13 +0300

    not solved

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -17,15 +17,31 @@
  */
 
 var DIGIT_VALUES = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
 };
 
 var translateRomanNumeral = function(romanNumeral){
-
+	var result = 0;
+	var keys = Object.keys(DIGIT_VALUES)
+	for (var i = 0; i < romanNumeral.length; i++) {
+		if(keys.indexOf(romanNumeral[i])===-1){
+			return null;
+		}
+		romanNumeral[i] = DIGIT_VALUES[romanNumeral[i]]
+		console.log(romanNumeral[i])
+		console.log(DIGIT_VALUES[romanNumeral[i]])
+		if(romanNumeral.length-1 !== romanNumeral[i])
+		if(romanNumeral[i] > romanNumeral[i+1]){
+			result+=romanNumeral[i]
+		}else{
+			result-=romanNumeral
+		}
+	}
+	return result;
 };
