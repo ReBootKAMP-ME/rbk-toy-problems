66f843015dcbe82d0cecab36115985a88c692451
Saif Elokour <saiffelokour@gmail.com>
2016-08-17 10:15:31 +0300

    solves tictactoe

--- tic-tac-toe/ticTacToe.js
+++ tic-tac-toe/ticTacToe.js
@@ -31,5 +31,56 @@ ticTacToe(solvedBoard);//should return 1
 */
 
 function ticTacToe(board){
+	var flag = false
+	var equalSum = function(a, b, c){
+		if((a === b) && (b === c) && ((a && b && c) > 0)){
+			return a
+		} else if ((a && b && c) > 0){
+			return 0
+		} else {
+			return -1
+		}
+	}
 
+	// rows
+	for(var i = 0; i < 3; i++){
+		var s = equalSum.apply(null, board[i])
+		if(s > 0){
+			return s
+		} else if (s < 0){
+			flag = true
+		}
+	}
+
+	// columns
+	for(var i = 0; i < 3; i++){
+		var s = equalSum.call(null, board[0][i], board[1][i], board[2][i])
+		if(s > 0){
+			return s
+		} else if (s < 0){
+			flag = true
+		}
+	}
+
+	// crosses
+	var crossL = equalSum.call(null, board[0][0], board[1][1], board[2][2])
+	var crossR = equalSum.call(null, board[0][2], board[1][1], board[2][0])
+
+	if(crossL > 0){
+		return crossL
+	} else if (crossL < 0){
+		flag = true
+	}
+
+	if(crossR > 0){
+		return crossR
+	} else if (crossR < 0){
+		flag = true
+	}
+
+	if(flag){
+		return -1
+	}
+
+	return 0
 };
