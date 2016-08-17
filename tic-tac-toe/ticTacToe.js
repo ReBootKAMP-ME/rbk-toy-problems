82407ace02d57f037981fe09da60fad0ee0f5ed6
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-17 09:38:23 +0300

    ticTacToe solution

--- tic-tac-toe/ticTacToe.js
+++ tic-tac-toe/ticTacToe.js
@@ -32,4 +32,41 @@ ticTacToe(solvedBoard);//should return 1
 
 function ticTacToe(board){
 
+	var conseqO = 0;
+	var conseqX = 0;
+	for(var i=0; i<board.length; i++){
+		var OnumH = 0, OnumV = 0, XnumH = 0, XnumV = 0;
+		for(var j=0; j<board.length; j++){
+			switch(board[i][j]){
+			case 1:
+				XnumH++;
+				break;
+			case 2:
+				OnumH++;
+				break;
+			}
+			switch(board[j][i]){
+			case 1:
+				XnumV++;
+				break;
+			case 2:
+				OnumV++;
+				break;
+			}
+		}
+		if(XnumH === 3 || XnumV === 3){
+			return 1;
+		}
+		if(OnumH === 3 || OnumV === 3){
+			return 2;
+		}
+	}
+	for(var i=0; i<board.length-1; i++){
+		if(board[i][i] !== board[i+1][i+1]){
+			break;
+		} else if(i=== board.length-2){
+			return board[i][i];
+		}
+	}
+	return -1;
 };
