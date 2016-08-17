0467c0388af5084249c11fafbf5ad4bfad4be508
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-17 09:48:59 +0300

    XO

--- tic-tac-toe/ticTacToe.js
+++ tic-tac-toe/ticTacToe.js
@@ -31,5 +31,70 @@ ticTacToe(solvedBoard);//should return 1
 */
 
 function ticTacToe(board){
-
+	for(var i=0;i<board.length;i++){
+		if(i===0){
+			if(board[0][0]===board[0][1] && board[0][1]===board[0][2]){
+				if(board[0][0]===1){
+					return 1;
+				}else if(board[0][0]===2){
+					return 2;
+				}
+			}
+			if(board[0][0]===board[1][0] && board[1][0]===board[2][0]){
+				if(board[0][0]===1){
+					return 1;
+				}else if(board[0][0]===2){
+					return 2;
+				}
+			}
+			if(board[0][0]===board[1][1] && board[1][1]===board[2][2]){
+				if(board[0][0]===1){
+					return 1;
+				}else if(board[0][0]===2){
+					return 2;
+				}
+			}
+			if(board[0][2]===board[1][1] && board[1][1]===board[2][0]){
+				if(board[0][2]===1){
+					return 1;
+				}else if(board[0][2]===2){
+					return 2;
+				}
+			}
+			if(board[0][1]===board[1][1] && board[1][1] ===board[2][1]){
+				console.log("second");
+				if(board[0][1]===1){
+					return 1;
+				}else if(board[0][1]===2){
+					return 2;
+				}
+			}
+			if(board[0][2]===board[1][2] && board[1][2]===board[2][2]){
+				if(board[0][2]===1){
+					return 1;
+				}else if(board[0][2]===2){
+					return 2;
+				}
+			}
+		}
+		if(i===1){
+			if(board[1][0]===board[1][1] && board[1][1]===board[1][2]){
+				if(board[1][0]===1){
+					return 1;
+				}else if(board[1][0]===2){
+					return 2;
+				}
+			}
+		}
+		if(i===2){
+			if(board[2][0]===board[2][1] && board[2][1]===board[2][2]){
+				if(board[2][0]===1){
+					return 1;
+				}else if(board[2][0]===2){
+					return 2;
+				}
+			}
+		}
+	}
+	return -1;
 };
