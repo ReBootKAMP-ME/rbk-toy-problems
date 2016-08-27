3b10fd784c7cf6ba7a78aa94436c30b60a63865d
farahamawi <amawifarah@gmail.com>
2016-08-27 09:45:05 +0300

    longestPalindrome solved

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -10,7 +10,37 @@
 * Extra credit: The best solution for this problem runs in linear time.
 * If you would like a challenge, read about Manacher's algorithm
 */
-
 var longestPalindrome = function (string) {
+	var palindrome = [];
+	var indexE = 0;
   
+  for (var s = 0; s < string.length; s++) {
+  	var loPal = function(indexE){
+  		var str1 = '';
+  		var str2 = '';
+		  for (var i = s; i < indexE; i++) {
+		  		str1 += string[i];
+		  }
+		  for (var i = indexE-1; i >= s; i--) {
+		  	str2 += string[i];
+		  }
+		  if(str1 === str2){
+		  	palindrome.push(str1)
+		  }
+		
+	  	if(indexE !== string.length-1){
+	  		loPal(indexE+1)
+	 		}			
+	  }
+	  loPal(indexE);
+	}
+	var counter = palindrome[0].length;
+	var result;
+	for (var i = 0; i < palindrome.length; i++) {
+		if(palindrome[i].length > counter){
+			counter = palindrome[i].length;
+			result = palindrome[i];
+		}
+	}
+  return result;
 };
