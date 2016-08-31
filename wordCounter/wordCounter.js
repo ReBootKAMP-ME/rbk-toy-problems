e6400255b3437dd3fb7b5aef59156fd24544b7dd
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-31 09:49:21 +0300

    wordCounter solution

--- wordCounter/wordCounter.js
+++ wordCounter/wordCounter.js
@@ -28,7 +28,44 @@ You can check your output with http://www.writewords.org.uk/word_count.asp
 fs = require('fs');
 
 var readFile = function (){
-  var contents = fs.readFileSync("./news.txt").toString();
-  return contents;
+  var contents = fs.readFileSync("./news.txt").toString().split(' ');
+  var words = {};
+  for(var i = 0; i < contents.length; i++){
+  	var word;
+  	if(contents[i].indexOf('.') !== -1){
+  		var temp = contents[i].split('.');
+  		for(var j=1; j<temp.length; j++){
+  			contents.push(temp[j]);
+  		}
+  		word = temp[0].split('');
+  	} else {
+  		word = contents[i].split('');
+  	}
+  	for(var j=0; j<word.length; j++){
+	  	if(word.indexOf(',') !== -1 ||
+	  		word.indexOf('\"') !== -1 ||
+	  		word.indexOf('\r') !== -1 ||
+	  		word.indexOf('\n') !== -1 ||
+	  		word.indexOf('\'') !== -1 ||
+	  		word.indexOf('') !== -1){
+	  		word.splice(j,1);
+	  		j--;
+	  	}
+	}
+	var resultWord = word.join('');
+	if(!words[resultWord]){
+		words[resultWord] = 0;
+	}
+	words[resultWord]++;
+  }
+  var result = {};
+  for(var key in words){
+  	if(words[key] === 1){
+  		result[key] = 1;
+  	}
+  }
+  return result;
 };
+
+console.log(readFile());
 
